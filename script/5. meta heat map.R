374e6d3d574d5b9fdfd9f8b896b7c3ef9b53f913
Administrator <admin@example.com>
2022-04-13 13:04:33 +0800

    heat map 4 cat

--- script/5. meta heat map.R
+++ script/5. meta heat map.R
@@ -134,3 +134,144 @@ bmi_heat <- ggplot()+
   ylab("Infant age\n")+
   xlab("Protein content (g/100 kcal)")
 bmi_heat
+
+
+# 2022 04 14 --------------------------------------------------------------
+
+# 重新对蛋白质进行分组
+
+mapData_2 <- readxl::read_excel("data/meta heat map data 2022 04 12-rqq.xlsx")
+syn_data_2 <- expand_grid(age = c(1:6), 
+                        protein = c("<1.8", "1.8~2", "2.1~2.2", ">2.2"))
+
+# weight gain
+mapData_wg2 <- filter(mapData_2, parameter == "weight gain")
+
+syn_data_wg2 <- left_join(syn_data_2, mapData_wg2, by =c("age", "protein"))
+
+syn_data_wg2 <- syn_data_wg2 %>% mutate(
+  effect = case_when(is.na(effect) ~ "miss",
+                     TRUE ~ effect)
+)
+
+wg_heat2 <- ggplot()+
+  geom_raster(data = syn_data_wg2, 
+              aes(x = protein, y = as.factor(age), fill = effect)) +
+  theme_bw() +
+  scale_fill_manual(name = "Mean difference",
+                    breaks = c("h","l","ns", "miss"),
+                    labels = c("Higher than BM", "Lower than BM",
+                               "Not significant", "No data"),
+                    values = c(h = "#b2182b",
+                               l = "#d9ef8b",
+                               ns = "#2166ac",
+                               miss = "#f7f7f7")) +
+  scale_x_discrete(labels = c("<1.8", "1.8~2", "2.1~2.2", ">2.2"),
+                   expand = c(0,0)) +
+  scale_y_discrete(breaks = c(1,2,3,4,5,6),
+                   labels = c("Month 1", "Month 2", "Month 3",
+                              "Month 4", "Month 5", "Month 6"),
+                   expand = c(0,0)) +
+  theme(
+    
+    axis.title = element_text(size = 20,
+                              family = "serif"),
+    axis.text = element_text(size = 15, 
+                             family = "serif"),
+    legend.title = element_text(size = 20,
+                                family = "serif"),
+    legend.text = element_text(size = 15, 
+                               family = "serif"),
+  ) +
+  ylab("Infant age\n")+
+  xlab("Protein content (g/100 kcal)")
+wg_heat2
+
+
+# height gain
+
+mapData_hg2 <- filter(mapData_2, parameter == "height gain")
+
+syn_data_hg2 <- left_join(syn_data_2, mapData_hg2, by =c("age", "protein"))
+
+syn_data_hg2 <- syn_data_hg2 %>% mutate(
+  effect = case_when(is.na(effect) ~ "miss",
+                     TRUE ~ effect)
+)
+
+hg_heat2 <- ggplot()+
+  geom_raster(data = syn_data_hg2, 
+              aes(x = protein, y = as.factor(age), fill = effect)) +
+  theme_bw() +
+  scale_fill_manual(name = "Mean difference",
+                    breaks = c("h","l","ns", "miss"),
+                    labels = c("Higher than BM", "Lower than BM",
+                               "Not significant", "No data"),
+                    values = c(h = "#b2182b",
+                               l = "#d9ef8b",
+                               ns = "#2166ac",
+                               miss = "#f7f7f7")) +
+  scale_x_discrete(labels = c("<1.8", "1.8~2", "2.1~2.2", ">2.2"),
+                   expand = c(0,0)) +
+  scale_y_discrete(breaks = c(1,2,3,4,5,6),
+                   labels = c("Month 1", "Month 2", "Month 3",
+                              "Month 4", "Month 5", "Month 6"),
+                   expand = c(0,0)) +
+  theme(
+    
+    axis.title = element_text(size = 20,
+                              family = "serif"),
+    axis.text = element_text(size = 15, 
+                             family = "serif"),
+    legend.title = element_text(size = 20,
+                                family = "serif"),
+    legend.text = element_text(size = 15, 
+                               family = "serif"),
+  ) +
+  ylab("Infant age\n")+
+  xlab("Protein content (g/100 kcal)")
+hg_heat2
+
+# BMI
+
+mapData_bmi2 <- filter(mapData_2, parameter == "bmi")
+
+syn_data_bmi2 <- left_join(syn_data_2, mapData_bmi2, by =c("age", "protein"))
+
+syn_data_bmi2 <- syn_data_bmi2 %>% mutate(
+  effect = case_when(is.na(effect) ~ "miss",
+                     TRUE ~ effect)
+)
+
+bmi_heat2 <- ggplot()+
+  geom_raster(data = syn_data_bmi2, 
+              aes(x = protein, y = as.factor(age), fill = effect)) +
+  theme_bw() +
+  scale_fill_manual(name = "Mean difference",
+                    breaks = c("h","l","ns", "miss"),
+                    labels = c("Higher than BM", "Lower than BM",
+                               "Not significant", "No data"),
+                    values = c(h = "#b2182b",
+                               l = "#d9ef8b",
+                               ns = "#2166ac",
+                               miss = "#f7f7f7")) +
+  scale_x_discrete(labels = c("<1.8", "1.8~2", "2.1~2.2", ">2.2"),
+                   expand = c(0,0)) +
+  scale_y_discrete(breaks = c(1,2,3,4,5,6),
+                   labels = c("Month 1", "Month 2", "Month 3",
+                              "Month 4", "Month 5", "Month 6"),
+                   expand = c(0,0)) +
+  theme(
+    
+    axis.title = element_text(size = 20,
+                              family = "serif"),
+    axis.text = element_text(size = 15, 
+                             family = "serif"),
+    legend.title = element_text(size = 20,
+                                family = "serif"),
+    legend.text = element_text(size = 15, 
+                               family = "serif"),
+  ) +
+  ylab("Infant age\n")+
+  xlab("Protein content (g/100 kcal)")
+bmi_heat2
